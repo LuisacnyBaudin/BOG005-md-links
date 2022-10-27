c25f18d90a896507adbabf81af2712d78ffb74dd
LuisacnyBaudin <luisacbaudin@gmail.com>
2022-10-26 20:06:26 -0500

    ayudantia de David

--- src/index.js
+++ src/index.js
@@ -1,57 +1,76 @@
 const chalk = require('chalk');
-const fs= require('fs')
+const fs = require('fs')
 const path = require('path');
-const {resolve}= require('path')
+const { marked } = require('marked');
+const { resolve } = require('path')
 
-//Convertir la ruta de relativa a absoluta
+//Convertir la ruta de relativa a absolut
+const processPath = process.argv[2];
 
 
-const pathAbsolute= (route) =>{
-const processPath= process.argv[2];
-const absoluteRoute= path.resolve(route).normalize();
-if(!path.isAbsolute(processPath)){
-console.log(chalk.magenta(absoluteRoute));
+const pathAbsolute = (route) => {
+  const absoluteRoute = path.resolve(route).normalize();
+  if (!path.isAbsolute(processPath)) {
     return absoluteRoute;
-}else{
+  } else {
     return processPath;
-} 
+  }
 };
 
 //utilizando extname para validar si el archivo es .md
-  const readextName = (processPath) => {
-    const fileMD = path.extname(pathAbsolute(processPath)) === '.md';
-    console.log(fileMD);
-    return fileMD;
+const readextName = (processPath) => {
+  const fileMD = path.extname(pathAbsolute(processPath)) === '.md';
+  return fileMD;
 };
 
 //Leer el archivo
-const readFiletwo = (processPath) =>{
-return new Promise((resolve, reject) =>{
-fs.readFile(processPath.path, 'utf8', (error, file) => {
-    if (error) {
-      throw error; 
-    }
-     else if(!readextName(processPath)) {
-      reject("ERROR")
-     }
-     resolve({
+const readFiletwo = (processPath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(processPath, 'utf8', (error, file) => {
+      console.log(fs.readFile(processPath));
+      if (error) {
+        throw error;
+      }
+      else if (!readextName(processPath)) {
+        reject("ERROR")
+      }
+      resolve({
         path: processPath,
-        file: file 
-     }
-     );
-  }); console.log(processPath)
-});
+        file: file
+      }
+      ); console.log(file);
+    });
+  });
 }
+
+// if(path.extname(path) == ".md" && fs.statSync(path).isFile()){
+//   arreglo.push(path);
+// }else{
+//   let direc = fs.readdirSync(newPath);
+//   direc.forEach((elemento) => {
+//     elemento = path.join(newPath, elemento);
+//     if (fs.statSync(elemento).isDirectory()) {
+//       arreglo = readdirec(elemento).concat(arreglo);
+//     } 
+
+// const isFile = fileName => {
+//   return fs.lstatSync(fileName).isFile();
+// };
+
+// fs.readdirSync(folderPath)
+//   .map(fileName => {
+//     return path.join(folderPath, fileName);
+//   })
+//   .filter(isFile);
+
 const markDownLinks = (processPath, options) => {
-    return new Promise((resolve, reject) => {
-        //Ingresa path
-        //Función para convertir la ruta en absoluta
-        const absoluteRoute = pathAbsolute(processPath);
-        //Función que evalua si la ruta es un archivo .md
-        readextName(absoluteRoute);
-        //Función que lee el archivo y crea el objeto
-        const basicInfoLinks = [];
-        readNewFile(absoluteRoute)
-        return basicInfoLinks;
-    })
+  return new Promise((resolve, reject) => {
+    const verifiAbsolute = pathAbsolute(processPath);
+    resolve(verifiAbsolute)
+  })
 }
+
+markDownLinks(processPath).then(processPath => {
+  console.log("si sirve", processPath);
+});
+
