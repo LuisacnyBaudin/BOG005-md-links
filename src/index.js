7378934d7857339e92586b6a7b0b79559484d89b
LuisacnyBaudin <luisacbaudin@gmail.com>
2022-10-28 20:30:49 -0500

    uso de marked para extraer links

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const chalk = require('chalk');
 const fs = require('fs')
 const path = require('path');
-const { marked } = require('marked');
-const { resolve } = require('path')
+const marked = require('marked');
+// const { resolve } = require('path')
 const routeTest = 'test(md)';
 
 //Convertir la ruta de relativa a absolut
@@ -18,71 +18,75 @@ const pathAbsolute = (route) => {
   }
 };
 
-function getMDfiles(allFileMD) { 
+function getMDfiles(allFileMD) {
   const isFile = fs.statSync(allFileMD).isFile();
   const isDirectory = fs.statSync(allFileMD).isDirectory();
   const readextName = path.extname(allFileMD);
-  let arrayMarkDown= [];
+  let arrayMarkDown = [];
   let pathContainer = pathAbsolute(allFileMD);
   // console.log(chalk.cyan(pathContainer));
   if (isFile && readextName === '.md') {
-      arrayMarkDown.push(pathContainer)
-  console.log(chalk.magenta(arrayMarkDown));
+    arrayMarkDown.push(pathContainer)
+    // console.log(chalk.magenta(arrayMarkDown));
   } else if (isFile && readextName !== '.md') {
-      console.log(chalk.red('Archivo no tiene extensión .md son', readextName))
-  } else if (isDirectory === true)   {
-      fs.readdirSync(allFileMD).forEach(file => {
-          let routePath = path.join(allFileMD,file);
-          console.log(fs.readdirSync(allFileMD))
-          console.log(chalk.magenta(routePath));
-          if (isDirectory) {
-            arrayMarkDown = arrayMarkDown.concat(getMDfiles(routePath))
-          } else {
-              if (path.extname(routePath) === '.md') {
-                arrayMarkDown.push(routePath)
-              }
-          }
-      })
+    // console.log(chalk.red('Archivo no tiene extensión .md son', readextName))
+  } else if (isDirectory === true) {
+    fs.readdirSync(allFileMD).forEach(file => {
+      let routePath = path.join(allFileMD, file);
+      // console.log(fs.readdirSync(allFileMD))
+      // console.log(chalk.magenta(routePath));
+      if (isDirectory) {
+        arrayMarkDown = arrayMarkDown.concat(getMDfiles(routePath))
+      } else {
+        if (path.extname(routePath) === '.md') {
+          arrayMarkDown.push(routePath)
+        }
+      }
+    })
   }
   return arrayMarkDown; // Retornando un array de archivos MD 
 }
-console.log(getMDfiles(routeTest));
 
-// //Leer el archivo
-// const readFiletwo = (processPath) => {
-//   return new Promise((resolve, reject) => {
-//     let prueba = [];
-//     fs.readFile(processPath, 'utf8', (err, data) => {
-//       console.log(fs.readFile(processPath));
-//       if (err) resolve(err);
-//       marked.marked(data, {
-//         walkTokens: (token) => {
-//           if (token.type === 'link' && token.href.includes('http')) {
-//             infoLink.push({
-//               href: token.href,
-//               text: token.text,
-//               file: processPath,
-//             })
-//           }
-//         }
-//       })
-//       resolve(prueba);
-//     })
-//   })
+const containerArray = getMDfiles(routeTest);
 
-// }
-// readFiletwo('src/README.md').then((val) => { console.log("probando", val) }) //.then dice que hacer cuando la promesa exitosa
-
-
-
-const markDownLinks = (processPath, options) => {
+const readMd = (file) => {
+  // "[hola](test) l l ll ll l ll [hola2](test2)".match(/\[([^\[\]]*)\]\((.*?)\)/g)
   return new Promise((resolve, reject) => {
-    const verificAbsolut = pathAbsolute(processPath);
-    resolve(verificAbsolut)
+    let pruebatwo = [];
+    fs.readFile(file, 'utf8', (err, datafile) => {
+      if (err) { resolve(err) };
+      const renderer = new marked.Renderer()
+      renderer.link = function (href, title, text) {
+        const creandoObjeto = {
+          'href': href,
+          'text': text,
+          'file': file,
+        }
+        if (creandoObjeto.href.includes('http')) {
+          pruebatwo.push(creandoObjeto);
+        }
+      }
+      marked.marked(datafile,
+        { renderer })
+      
+      resolve(pruebatwo);
+    });
+  })  
+}  
+
+// readMd(processPath).then((val) => { console.log("probando", val) })
+
+
+const markDownLinks = (Path, option) => {
+  return new Promise((resolve, reject) => {
+    const verifyAbsolut = pathAbsolute(Path);
+    const containerArray = getMDfiles(verifyAbsolut);
+    const readMarkdown = readMd(containerArray);
+    resolve(readMarkdown)
   })
 }
 
-markDownLinks(processPath).then(processPath => {
-  console.log("si sirve", processPath);
+markDownLinks(routeTest).then((data) => {
+  console.log("si sirve", data);
 });
 
