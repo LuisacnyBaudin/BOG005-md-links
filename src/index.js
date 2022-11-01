105c85cc55b2b187cff3f6af34645e9e22f55f25
LuisacnyBaudin <luisacbaudin@gmail.com>
2022-11-01 12:31:08 -0500

    validatelinks in true o false

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 const fs = require('fs')
 const path = require('path');
 const marked = require('marked');
-const axios = require('axios')
+const axios = require('axios');
 const routeTest = 'testmd';
 
 //Convertir la ruta de relativa a absolut
@@ -52,41 +52,75 @@ const containerArray = getMDfiles(routeTest);
 const readMd = (file) => {
   // "[hola](test) l l ll ll l ll [hola2](test2)".match(/\[([^\[\]]*)\]\((.*?)\)/g)
   return new Promise((resolve, reject) => {
-    let pruebatwo = [];
+    let arrayObjects = [];
     fs.readFile(file, 'utf8', (err, datafile) => {
       if (err) { resolve(err) };
       const renderer = new marked.Renderer()
       renderer.link = function (href, title, text) {
-        const creandoObjeto = {
+        const objectContainer = {
           'href': href,
           'text': text,
           'file': file,
         }
-        if (creandoObjeto.href.includes('http')) {
-          pruebatwo.push(creandoObjeto);
+        if (objectContainer.href.includes('http')) {
+          arrayObjects.push(objectContainer);
         }
       }
       marked.marked(datafile,
         { renderer })
       
-      resolve(pruebatwo);
+      resolve(arrayObjects);
     });
   })  
 }  
 const getAllobjects = (arraysMD) => {
 const returnPromise= arraysMD.map(file => readMd(file));
-return Promise.all(returnPromise).then(res => res);
+return Promise.all(returnPromise).then(res => res.flat());
 }
-
 // readMd(processPath).then((val) => { console.log("probando", val) })
 
+function processLink(link) {
+  return new Promise((resolve, reject) => {
+    axios.get(link.href)
+      .then((response) => {
+        if(response.status >= 200 && response.status < 400){
+        link.status = response.status
+        link.ok = '✅';
+        resolve(link);
+         }
+      }) .catch((error) => {
+        let status = 500; 
+        if(error.response){
+          status = error.response.status
+        }else if(error.request){
+          status = 503;
+        }
+        link.status = error.status
+        link.ok = '🚫';
+        resolve(link);
+      }); 
+    })
+    
+}
+  const getvalidateLinks = (validateLinks) => {
+    let returnValidateLinks= validateLinks.map(link => processLink(link));
+  return Promise.all(returnValidateLinks).then(res => res);
+  }
 
-const markDownLinks = (Path, option) => {
+
+
+const markDownLinks = (Path, option={validate:true}) => {
   return new Promise((resolve, reject) => {
     const verifyAbsolut = pathAbsolute(Path);
     const containerArray = getMDfiles(verifyAbsolut);
-    const readMarkdown = getAllobjects(containerArray);
-    resolve(readMarkdown)
+   if(option.validate){
+    getAllobjects(containerArray)
+    .then(res => getvalidateLinks(res))
+    .then(res2 => resolve(res2))
+   }else {
+    getAllobjects(containerArray)
+    .then(res => resolve(res))
+   }
   })
 }
 
